faf1e121a05e57bce02d465970546ffa856391fe
Marko Bogosavljevic <7422135+ThatMarko@users.noreply.github.com>
2022-10-15 15:53:51 -0500

    Adress the color name syntax. Add comments regarding defined CSS variables.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,17 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     colors: {
-      primary: "rgb(var(--color-primary) / <alpha-value>)",
-      primaryLight: "rgb(var(--color-primary-light) / <alpha-value>)",
-      primaryDark: "rgb(var(--color-primary-dark) / <alpha-value>)",
-
-      secondaryLight: "rgb(var(--color-secondary-light) / <alpha-value>)",
-      secondaryDark: "rgb(var(--color-secondary-dark) / <alpha-value>)",
+      // Defines primary, primary-light, primary-dark
+      primary: {
+        light: "rgb(var(--color-primary-light) / <alpha-value>)",
+        DEFAULT: "rgb(var(--color-primary) / <alpha-value>)",
+        dark: "rgb(var(--color-primary-dark) / <alpha-value>)",
+      },
+      // Defines secondary-light and secondary-dark
+      secondary: {
+        light: "rgb(var(--color-secondary-light) / <alpha-value>)",
+        dark: "rgb(var(--color-secondary-dark) / <alpha-value>)",
+      },
     },
     extend: {},
   },
