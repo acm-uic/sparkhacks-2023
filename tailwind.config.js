9acdb9476c637f8fe95074940ddb0d954b884e85
Marko Bogosavljevic <7422135+ThatMarko@users.noreply.github.com>
2022-10-15 13:26:15 -0500

    Define primary and secondary colors as css variables. Define header pt and weight styling and body font size. Fixes #56

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      primary: "rgb(var(--color-primary) / <alpha-value>)",
+      primaryLight: "rgb(var(--color-primary-light) / <alpha-value>)",
+      primaryDark: "rgb(var(--color-primary-dark) / <alpha-value>)",
+
+      secondaryLight: "rgb(var(--color-secondary-light) / <alpha-value>)",
+      secondaryDark: "rgb(var(--color-secondary-dark) / <alpha-value>)",
+    },
     extend: {},
   },
   plugins: [],
-}
+};
