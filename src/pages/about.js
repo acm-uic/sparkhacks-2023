fd1eaeda4b089ed7bc84014e4ff15983f4ea5b47
Christian Bingman <42191425+ChristianBingman@users.noreply.github.com>
2022-10-17 17:08:09 -0500

    Revert "Issue #53 - Add speakers section"

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,5 @@
 import './about.css'
-import SpeakersSection from '../components/SpeakersSection';
 
 export default function About(){
-    return (<div>
-                <h1>About</h1>
-                <SpeakersSection/>
-            </div>
-    );
+    return (<h1>About</h1>)
 }
