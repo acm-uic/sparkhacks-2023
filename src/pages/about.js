37598a112a62224ccaf365b7d4dbb478f6225db3
rapterjet2004 <juliuslinus1@gmail.com>
2022-10-17 15:09:44 -0500

    Reworks to issue #53

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,10 @@
 import './about.css'
+import SpeakersSection from '../components/SpeakersSection';
 
 export default function About(){
-    return (<h1>About</h1>)
+    return (<div>
+                <h1>About</h1>
+                <SpeakersSection/>
+            </div>
+    );
 }
