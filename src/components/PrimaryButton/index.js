0258019d502432a73e71e58874bd5fdbe5a8606f
Eduardo Morales <emoral435@gmail.com>
2022-10-16 23:01:18 -0500

    Fixes issue #57
    Added Primary Button and Primary Button (hovering)

--- src/components/PrimaryButton/index.js
+++ src/components/PrimaryButton/index.js
@@ -1,5 +1,5 @@
 export default function PrimaryButton(props) {
-    return (<button className='rounded-[40px] bg-primary-light text-white font-bold hover:bg-primary-dark py-3 px-3 w-fit'>
+    return (<button className='rounded-[40px] bg-primary-light text-white font-bold hover:bg-primary-dark py-2 px-6 w-fit'>
               <h3>{props.innerText}</h3>
             </button>)
   };
