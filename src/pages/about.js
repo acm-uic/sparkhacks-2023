4dcc89974a61fa9f4dc5c56f50b2c1e0ff541138
Adrian Knight <aknig2@uic.edu>
2022-10-19 15:06:12 -0500

    Progress on "Who We Are" section in about page with tailwind

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,11 +2,10 @@ import './about.css'
 import SpeakersSection from '../components/SpeakersSection';
 
 export default function About(){
-    return (<div>
-                <h1>About</h1>
-                <div className={"who-we-are"}>
-                    <h1>Who We Are</h1>
-                    <div className={"text"}>
+    return (<div className={"place-items-center"}>
+                <div className={"container mx-auto"}>
+                    <div className={"font-bold text-[38px] mx-auto"}>Who We Are</div>
+                    <div className={"text-base text-[14px] mx-[10rem]"}>
                         The Flourish! Council is a small group of ~10 students who are interested in open source technologies and want to bring this knowledge to other students! We are all UIC students ranging from 1st year to graduate with varying levels of experience. All with a common goal in of teaching about open source! Our SIG WebDev created this site as our first official project aimed at increasing interest through a fun and informative website! See below for more information about the executive project leaders and our amazing speakers!
                     </div>
                 </div>
