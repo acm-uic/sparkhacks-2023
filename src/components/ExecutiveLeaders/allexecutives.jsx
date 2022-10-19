e45e5ab7ccd3485f512c0a0a5b4d2682af834421
rapterjet2004 <juliuslinus1@gmail.com>
2022-10-18 22:07:48 -0500

    Add executive leaders section to About Us Fixes #52

---
+++ src/components/ExecutiveLeaders/allexecutives.jsx
@@ -0,0 +1,5 @@
+export const IMAGE_LINKS = [
+    "https://user-images.githubusercontent.com/42191425/195487234-8b413ac2-a707-4295-8488-5ee94fe52b04.png",
+    "https://user-images.githubusercontent.com/42191425/195487251-ad211be3-3f03-4486-9ccd-3d4b1bf6238a.png",
+    "https://user-images.githubusercontent.com/42191425/195487266-4d3e9ae3-4fcc-4503-b196-ef00b495ce4f.png"
+];
