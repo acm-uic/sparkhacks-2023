8d77cb5bc73b23f42719ddc2cba45431d15edd74
Jeongjae-0124 <brianjeongjae123@gmail.com>
2022-10-23 16:10:29 -0500

    Fix #50 -Create a Description section of About Us
    resolved minor mistakes #50

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,7 @@
 import './about.css'
 import SpeakersSection from '../components/SpeakersSection';
 import ExecutiveLeaders from '../components/ExecutiveLeaders';
-import Descpription from '../components/Description';
+import Description from '../components/Description';
 export default function About(){
 
     return (<div className={"place-items-center m-auto"}>
@@ -10,10 +10,10 @@ export default function About(){
                         <h1>Who We Are</h1>
                     </div>
                     <div className={"ml-[10rem] min-w-fit"}>
-                        <body>The Flourish! Council is a small group of ~10 students who are interested in open source technologies and want to bring this knowledge to other students! We are all UIC students ranging from 1st year to graduate with varying levels of experience. All with a common goal in of teaching about open source! Our SIG WebDev created this site as our first official project aimed at increasing interest through a fun and informative website! See below for more information about the executive project leaders and our amazing speakers!</body>
+                        <p>The Flourish! Council is a small group of ~10 students who are interested in open source technologies and want to bring this knowledge to other students! We are all UIC students ranging from 1st year to graduate with varying levels of experience. All with a common goal in of teaching about open source! Our SIG WebDev created this site as our first official project aimed at increasing interest through a fun and informative website! See below for more information about the executive project leaders and our amazing speakers!</body>
                     </div>
                 </div>
-                <Descpription/>
+                <Description/>
                 <ExecutiveLeaders/>
                 <SpeakersSection/>
             </div>
