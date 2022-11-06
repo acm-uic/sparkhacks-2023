0751cc637e9522055e9b0a2aa90a088b46a38cd7
Adrian Knight <aknig2@uic.edu>
2022-11-05 19:14:50 -0500

    Removed italics

--- src/components/Description/index.jsx
+++ src/components/Description/index.jsx
@@ -5,7 +5,7 @@ export default function Description(){
                         <h1>Description</h1>
                     </div>
                     <div className={"sm:ml-[10rem] min-w-fit"}>
-                        <p><i>Flourish!</i> is an annually held conference intended to introduce both CS and non-CS, technical and non-technical majors to the world of open source through speakers, demos, workshops, sponsors, job opportunities, and other events! We show off hardware, software, business, entrepreneurship, finance, and more aspects of open source. We want to make this project bigger and bigger year over year and bring in new students. Our previous conferences focused on a small number of students in a technical software direction, but we are looking to change that!</p>
+                        <p>Flourish! is an annually held conference intended to introduce both CS and non-CS, technical and non-technical majors to the world of open source through speakers, demos, workshops, sponsors, job opportunities, and other events! We show off hardware, software, business, entrepreneurship, finance, and more aspects of open source. We want to make this project bigger and bigger year over year and bring in new students. Our previous conferences focused on a small number of students in a technical software direction, but we are looking to change that!</p>
                     </div>
             </div>
            
