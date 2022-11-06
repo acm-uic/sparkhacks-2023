4f075f28c0c54d3ac22a832cf7b09d50a35103c5
Eduardo Morales <emoral435@gmail.com>
2022-11-05 20:36:46 -0500

    Fixes #71
    Fixes prop type nonsense, and also added a file to keep track of google maps links. Instead of getting the longitude and latitude (which you get from google maps anyways), you just get the link

---
+++ src/components/ShowMap/googleMapsLinks.jsx
@@ -0,0 +1,3 @@
+export const MAP_LINKS = [
+    'https://www.google.com/maps/place/University+of+Illinois+Chicago/@41.8685636,-87.6506384,17z/data=!3m1!4b1!4m5!3m4!1s0x880e2ce01f97c2bb:0xa6033e9aa2dbdcdc!8m2!3d41.8685636!4d-87.6484497'
+]
