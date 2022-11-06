5ab6fe22b9f845cd75d4cd31759af0a30d5e73eb
Adrian Knight <aknig2@uic.edu>
2022-11-05 19:07:57 -0500

    Changed text from body to p

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,7 +10,7 @@ export default function About(){
                         <h1>Who We Are</h1>
                     </div>
                     <div className={"ml-[10rem] min-w-fit"}>
-                        <body>The Flourish! Council is a small group of ~10 students who are interested in open source technologies and want to bring this knowledge to other students! We are all UIC students ranging from 1st year to graduate with varying levels of experience. All with a common goal in of teaching about open source! Our SIG WebDev created this site as our first official project aimed at increasing interest through a fun and informative website! See below for more information about the executive project leaders and our amazing speakers!</body>
+                        <p>The Flourish! Council is a small group of ~10 students who are interested in open source technologies and want to bring this knowledge to other students! We are all UIC students ranging from 1st year to graduate with varying levels of experience. All with a common goal in of teaching about open source! Our SIG WebDev created this site as our first official project aimed at increasing interest through a fun and informative website! See below for more information about the executive project leaders and our amazing speakers!</p>
                     </div>
                 </div>
                 <Description/>
