9b41c50a15deaf0905be7fd69b0519556df591f7
Eduardo Morales <emoral435@gmail.com>
2022-11-10 22:06:23 -0600

    Fixes #71
    Fixed eslint error

--- src/components/ShowMap/index.jsx
+++ src/components/ShowMap/index.jsx
@@ -1,24 +1,15 @@
 import PrimaryButton from "../PrimaryButton";
-import PropTypes from 'prop-types';
+import { LOCATION_LINKS } from "./Locations";
 
 export default function ShowMap(props) {
     const openMap = () => {
+        const index = props.index
         const windowSize = 'width' + window.innerWidth + ',height=' + window.innerHeight + ",scrollbars=no"
-        const url = `https://maps.google.com?q=${props.lat},${props.lng}`
+        const url = LOCATION_LINKS[index]
         window.open(url, 'popup', windowSize)
     };
     return (
         <div onClick={openMap}>
             <PrimaryButton innerText='Show on Map' />
         </div>)
-}
-
-ShowMap.propTypes = {
-    lat: PropTypes.number,
-    lng: PropTypes.number,
-}
-
-ShowMap.defaultTypes = {
-    lat: 41.8685636,
-    lng: -87.6506384,
 }
