c1e2764abdb429b397106ee2f62314d7ae70eaa2
Eduardo Morales <emoral435@gmail.com>
2022-11-10 21:49:27 -0600

    Fixes #71
    I fixed the eslint error that was popping up because of the primary button file

--- src/components/PrimaryButton/index.js
+++ src/components/PrimaryButton/index.js
@@ -1,7 +1,5 @@
-import PropTypes from 'prop-types';
-
 export default function PrimaryButton(props) {
     return (<button className='rounded-[40px] bg-primary-light text-white font-bold hover:bg-primary-dark py-2 px-6 w-fit' style={{ maxHeight: '75px'}}>
               <h3>{props.innerText}</h3>
             </button>)
-  };
+  }
