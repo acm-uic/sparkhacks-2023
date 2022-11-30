6a6b2ec5512beed18a4a10b9bffda13da8d9ecfe
Adrian Knight <aknig2@uic.edu>
2022-11-30 16:58:36 -0600

    Completed tailwind styling
    Date and Time swap position and title renders below date.

--- src/components/EventCard/index.jsx
+++ src/components/EventCard/index.jsx
@@ -1,17 +1,20 @@
 import PrimaryButton from "../PrimaryButton";
 
+// const renderMobile = (screenSize) > {
+//
+// }
 export default function EventCard({ event }) {
   return (
     <div>
-      <div className="flex flex-row-reverse lg:flex-row w-full py-1 items-center">
+      <div className="flex justify-between content-start flex-row-reverse lg:flex-row w-full py-1">
         <h4 className="flex">{event.time}</h4>
-        <h2 className="hidden lg:block lg:flex-auto lg:ml-4">{event.header}</h2>
-        <h4 className="flex-initial lg:justify-self-end">{event.date}</h4>
+        <h2 className="hidden lg:block flex-auto lg:ml-4">{event.header}</h2>
+        <h4 className="justify-self-end">{event.date}</h4>
       </div>
       <h2 className="lg:hidden flex-auto">{event.header}</h2>
-      <div className="flex sm:py-1 items-center">
+      <div className="flex py-1 items-center">
         <p className="pr-10">{event.description}</p>
-        <div className="hidden lg:block flex-none justify-self-end">
+        <div className="hidden lg:block flex-none  justify-self-end">
           <PrimaryButton innerText={"Show on Map"} />
         </div>
       </div>
