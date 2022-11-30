26bcf5bc318ed3898418a6d7fa490027d3e37ef0
Adrian Knight <aknig2@uic.edu>
2022-11-30 16:35:12 -0600

    Implemented styling

--- src/components/EventCard/index.jsx
+++ src/components/EventCard/index.jsx
@@ -3,14 +3,15 @@ import PrimaryButton from "../PrimaryButton";
 export default function EventCard({ event }) {
   return (
     <div>
-      <div className="flex w-full py-1 items-center">
-        <h4 className="flex-initial">{event.time}</h4>
-        <h2 className="flex-auto ml-4">{event.header}</h2>
-        <h4 className="flex-initial justify-self-end">{event.date}</h4>
+      <div className="flex flex-row-reverse lg:flex-row w-full py-1 items-center">
+        <h4 className="flex">{event.time}</h4>
+        <h2 className="hidden lg:block lg:flex-auto lg:ml-4">{event.header}</h2>
+        <h4 className="flex-initial lg:justify-self-end">{event.date}</h4>
       </div>
-      <div className="flex py-1 items-center">
+      <h2 className="lg:hidden flex-auto">{event.header}</h2>
+      <div className="flex sm:py-1 items-center">
         <p className="pr-10">{event.description}</p>
-        <div className="flex-none justify-self-end">
+        <div className="hidden lg:block flex-none justify-self-end">
           <PrimaryButton innerText={"Show on Map"} />
         </div>
       </div>
