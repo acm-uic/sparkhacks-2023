0580f33e88819ae9062d11c9185995d1dcdc88e6
kao <trumblays@yahoo.com>
2022-12-07 16:48:01 -0600

    adding styling

--- src/pages/social.js
+++ src/pages/social.js
@@ -1,13 +1,13 @@
-import './social.css'
+import './social.css';
 import ArchiveCarousel from '../components/ArchiveCarousel';
+import InputTextbox from '../components/InputTextbox';
 
-
-export default function Social(){
-    return (
-        <div>
-            <h1>Social</h1>
-            <ArchiveCarousel/>
-        </div>
-    
-    );
+export default function Social() {
+  return (
+    <div>
+      <h1>Social</h1>
+      <InputTextbox />
+      <ArchiveCarousel />
+    </div>
+  );
 }
