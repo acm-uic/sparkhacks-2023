b3cf1a1d534e216b2c7063bafe344cafc5f94593
kao <trumblays@yahoo.com>
2022-12-09 18:59:23 -0600

    rm excess code and added global styling.

--- src/components/InputTextbox/index.jsx
+++ src/components/InputTextbox/index.jsx
@@ -1,38 +1,18 @@
 function index() {
   return (
-    <section className='grid place-items-center bg-primary h-[700px]'>
-      <form
-        action=''
-        method='post'
-        className='w-6/12 rounded-[50px] h-96 bg-secondary-light p-10 z-10'
-      >
-        <h2>Join Our Newletter!</h2>
-        <p>
-          Want to get updates as fast as possible! Join our newsletter! We will
-          send details about the event with sign up and RSVP forms as we get
-          closer to the start date! We will also send insights into what types
-          of events we are planning, and much more! Sign up to make sure you get
-          the latest as soon as possible!
-        </p>
-        <div className='h-10 mt-5'>
+    <section>
+      <form action='' method='post'>
+        <div>
           <input
             type='text'
             value=''
             placeholder='Email:'
-            className='w-[70%] h-10 rounded-full p-5 mr-3 border-2 border-secondary-dark	'
+            className='input-field'
           />
-          <input
-            type='submit'
-            value='Submit'
-            className='bg-primary-light h-10 rounded-full w-40 font-bold text-white '
-          />
+          <input type='submit' value='Submit' className='input-field' />
         </div>
       </form>
-      <form
-        action=''
-        method='post'
-        className='w-6/12 h-96 rounded-[50px] bg-primary-dark p-16 absolute -translate-x-16 -translate-y-10 z-0'
-      ></form>
+      <form action='' method='post'></form>
     </section>
   );
 }
--- src/pages/social.js
+++ src/pages/social.js
@@ -1,12 +1,10 @@
 import './social.css';
 import ArchiveCarousel from '../components/ArchiveCarousel';
-import InputTextbox from '../components/InputTextbox';
 
 export default function Social() {
   return (
     <div>
       <h1>Social</h1>
-      <InputTextbox />
       <ArchiveCarousel />
     </div>
   );
