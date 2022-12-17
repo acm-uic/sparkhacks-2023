a9ad11d6de4f3c171e3f59b9f7dbb13054bc0cb4
kao <trumblays@yahoo.com>
2022-12-17 14:06:30 -0600

    rm state and handleChange, Used props.

--- src/components/InputTextbox/index.jsx
+++ src/components/InputTextbox/index.jsx
@@ -1,18 +1,11 @@
-import { useState } from "react";
-
 export default function index(props) {
-  const [email, setEmail] = useState('');
-
-  const handleChange = e => {
-    setEmail(e.target.value);
-  };
 
   return (
     <div>
       <input
         type='text'
-        onChange={handleChange}
-        value={email}
+        onChange={props.onChange}
+        value={props.onChange}
         placeholder={props.placeholder}
         className='input-field'
       />
--- src/pages/social.js
+++ src/pages/social.js
@@ -2,13 +2,12 @@ import './social.css';
 import ArchiveCarousel from '../components/ArchiveCarousel';
 import DesktopSMBubbles from '../components/DesktopSMBubbles';
 
-export default function Social(){
+export default function Social() {
   return (
-      <div>
-          <h1>Social</h1>
-          <DesktopSMBubbles/>
-          <ArchiveCarousel/>
-      </div>
-
+    <div>
+      <h1>Social</h1>
+      <DesktopSMBubbles />
+      <ArchiveCarousel />
+    </div>
   );
 }
