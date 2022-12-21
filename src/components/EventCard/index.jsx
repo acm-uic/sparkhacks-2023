426ea209a2231986e3fdc5f7adbd279edceda34b
rapterjet2004 <juliuslinus1@gmail.com>
2022-12-21 13:17:04 -0600

    manually fixing weird errros

--- src/components/EventCard/index.jsx
+++ src/components/EventCard/index.jsx
@@ -1,16 +1,18 @@
 import PrimaryButton from "../PrimaryButton";
 
+
 export default function EventCard({ event }) {
   return (
     <div className="px-2">
-      <div className="flex w-full py-1 items-center">
-        <h4 className="flex-initial">{event.time}</h4>
-        <h2 className="flex-auto ml-4">{event.header}</h2>
-        <h4 className="flex-initial justify-self-end">{event.date}</h4>
+      <div className="flex justify-between flex-row-reverse lg:flex-row w-full items-center py-1">
+        <h4 className="">{event.time}</h4>
+        <h2 className="hidden lg:block flex-auto ml-4">{event.header}</h2>
+        <h4 className="">{event.date}</h4>
       </div>
+      <h2 className="lg:hidden flex-auto">{event.header}</h2>
       <div className="flex py-1 items-center">
         <p className="pr-10">{event.description}</p>
-        <div className="flex-none justify-self-end">
+        <div className="hidden lg:block flex-none justify-self-end">
           <PrimaryButton innerText={"Show on Map"} />
         </div>
       </div>
