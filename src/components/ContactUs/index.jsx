678706a59ed01779e08dfd9d5c0e2932d9c08fcc
Adrian Knight <aknig2@uic.edu>
2023-01-09 23:33:53 -0600

    Styling completed
    Added state for select and added CSS to control color for select

--- src/components/ContactUs/index.jsx
+++ src/components/ContactUs/index.jsx
@@ -1,13 +1,23 @@
 import PrimaryButton from "../PrimaryButton";
+import React from "react";
 
 export default function ContactUs(){
+    const [value, setValue] = React.useState('');
+    const handleChange = (event) => {
+        setValue(event.target.value);
+    };
     return (
         <div className={"text-center"}>
             <h1>Contact Us!</h1>
             <div className="grid grid-flow-row w-full gap-3">
-                <input type="text" placeholder="Name:" />
-                <input type="text" placeholder="Reason:" />
-                <textarea type="textarea" rows={5} placeholder="Additional Details:" />
+                <input type="text" placeholder="Name" />
+                <select required value={value} onChange={handleChange}>
+                    <option value="" hidden>Reason</option>
+                    <option value={"Sponsorship"}>Interested in sponsoring</option>
+                    <option value={"Speaker"}>Interested in being a speaker</option>
+                    <option value={"Other"}>Other</option>
+                </select>
+                <textarea type="textarea" rows={5} placeholder="Additional Details" />
                 <PrimaryButton className={"w-full"} innerText="Send"/>
             </div>
         </div>
--- src/components/RsvpForm/index.jsx
+++ src/components/RsvpForm/index.jsx
@@ -1,13 +1,18 @@
 import PrimaryButton from "../PrimaryButton";
+import React from "react";
 
 export default function RsvpForm(){
+    const [value, setValue] = React.useState('');
+    const handleChange = (event) => {
+        setValue(event.target.value);
+    };
     return (
         <div className={"text-center"}>
             <h1>Want to Attend?</h1>
             <div className="grid grid-flow-row content-center w-full gap-3">
                 <input type="text" placeholder="Name" />
-                <select value={"Major"}>
-                    <option value="none" selected hidden>Major</option>
+                <select required value={value} onChange={handleChange}>
+                    <option value="" hidden>Major</option>
                     <option value={"Computer Science"}>Computer Science</option>
                     <option value={"Computer Engineering"}>Computer Engineering</option>
                     <option value={"Other"}>Other</option>
