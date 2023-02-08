1fc0aab826f09de0b69b3f61eb43420bf1b3ccab
Adrian Knight <aknig2@uic.edu>
2023-02-08 16:39:42 -0600

    Background image formatted, min size for contact us

--- src/components/ContactUs/index.jsx
+++ src/components/ContactUs/index.jsx
@@ -7,7 +7,7 @@ export default function ContactUs(){
         setValue(event.target.value);
     };
     return (
-        <div className={"grid h-screen bg-primary place-items-center"}>
+        <div className={"grid h-screen bg-primary place-items-center mt-5"}>
             <div className="absolute h-fit w-1/2 min-w-max px-7 py-10 drop-shadow-[-90px_-60px_4px_rgba(0,0,0,0.5)] bg-secondary-light rounded-[2.813rem]">
                 <h1>Contact Us!</h1>
                 <div className="grid grid-flow-row gap-1 w-full min-w-max p-3">
--- src/components/RsvpForm/index.jsx
+++ src/components/RsvpForm/index.jsx
@@ -7,26 +7,34 @@ export default function RsvpForm(){
         setValue(event.target.value);
     };
     return (
-        <div className={"text-center sm:bg-primary box-border py-3"}>
-            <h1 className={"sm:text-white text-black"}>Want to Attend?</h1>
-            <div className={"flex items-center justify-center "}>
-                <div className="grid grid-flow-row w-full sm:max-w-2xl gap-3 p-3 m-auto">
-                    <input type="text" placeholder="Name" />
-                    <div className={"drop-down"}>
-                        <div className={"arrow-down"}/>
-                        <select required value={value} onChange={handleChange}>
-                            <option value="" hidden>Major</option>
-                            <option value={"Computer Science"}>Computer Science</option>
-                            <option value={"Computer Engineering"}>Computer Engineering</option>
-                            <option value={"Other"}>Other</option>
-                        </select>
-                    </div>
+        <div className={"text-center box-border py-3"}>
+            <div className={"relative block"}>
+                <div className={"block relative box-border w-fit overflow-hidden max-h-[410px]"}>
+                    <img className={"hidden sm:block relative"} src={require("../../assets/images/archivedImages/2018.png")}/>
+                </div>
+                <div className={"sm:absolute top-0 mx-auto block p-2 w-full"}>
+                    <h1 className={"sm:text-white text-black"}>Want to Attend?</h1>
+                    <div className={"flex items-center justify-center "}>
+                        <div className="grid grid-flow-row w-full sm:max-w-2xl gap-3 p-3 m-auto">
+                            <input type="text" placeholder="Name" />
+                            <div className={"drop-down"}>
+                                <div className={"arrow-down"}/>
+                                <select required value={value} onChange={handleChange}>
+                                    <option value="" hidden>Major</option>
+                                    <option value={"Computer Science"}>Computer Science</option>
+                                    <option value={"Computer Engineering"}>Computer Engineering</option>
+                                    <option value={"Other"}>Other</option>
+                                </select>
+                            </div>
 
-                    <input type="text" placeholder="University" />
-                    <input type="text" placeholder="Email" />
-                    <PrimaryButton className={"w-full"} innerText="RSVP"/>
+                            <input type="text" placeholder="University" />
+                            <input type="text" placeholder="Email" />
+                            <PrimaryButton className={"w-full"} innerText="RSVP"/>
+                        </div>
+                    </div>
                 </div>
             </div>
+
 
         </div>
 
