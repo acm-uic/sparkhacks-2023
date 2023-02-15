96c79549fa419ab4ea9d6a5b307839d8c3fa72ce
Marko Bogosavljevic <7422135+ThatMarko@users.noreply.github.com>
2023-02-15 17:41:36 -0600

    Mobile day selector (events) completed with animations [ThatMarko/issue70] (#123)
    * Optimize events for large screens. Format code.
    
    * Format code, justify text (as per design guide)
    
    * Ceate the day selector for the event schedule for mobile, fix #70
    
    * Fix selected day background
    
    * Fix overflow hiding content when more than 2 events in a day.
    
    * Focus the first day (index=0)
    
    * Revert edit of gitignore
    
    * Fix animations
    
    * Tailwind optimizations

--- src/components/Events/index.jsx
+++ src/components/Events/index.jsx
@@ -1,55 +1,76 @@
 import { useState } from "react";
 import Day from "./Day";
 import EventCard from "../EventCard";
-
+import MobileDay from "./MobileDay";
 //List of days
 const days = [
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-    "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
 ];
 
 // Helper function to map all events from listOfEvents to EventCards
 
 function showEvents(listOfEvents) {
-    return listOfEvents.map((event, i, arr) => (
-
-        // Don't add <hr /> to the last element
-        (arr.length == i + 1) ? <EventCard event={event} key={event.id} /> : <> <EventCard event={event} key={event.id} /> <hr className="m-2 " /> </>
-    ));
+  return listOfEvents.map((event, i, arr) =>
+    // Don't add <hr /> to the last element
+    arr.length == i + 1 ? (
+      <EventCard event={event} key={event.id} />
+    ) : (
+      <>
+        <EventCard event={event} key={event.id} /> <hr className="m-2 " />
+      </>
+    )
+  );
 }
+
+function showNone() {
+  return <div>No events</div>;
+}
+
 export default function Events({ events }) {
+  //Track active day; Monday is selected by default;
 
-    //Track active day; Monday is selected by default;
+  const [active, setActive] = useState(days[0]);
 
-    const [active, setActive] = useState(days[0]);
+  return (
+    <>
+      {/* Large and above screen */}
+      <div className="hidden lg:block border-solid border-4 border-primary-dark min-w-full">
+        {/* Day Selector */}
 
-    return (
-        <div className="border-solid border-4 border-primary-dark">
-
-            {/* Day Selector */}
-
-            <div className="grid grid-flow-col">
-                {days.map((day) => (
-                    <Day
-                        key={day}
-                        text={day}
-                        isActive={active === day}
-                        handleClick={() => setActive(day)}
-                    >
-                        {day}
-                    </Day>
-                ))}
-            </div>
-
-            {/* Events of the selected day */}
-
-            <div> {events[active] ? showEvents(events[active]) : "No events"} </div>
-
+        <div className="grid grid-flow-col">
+          {days.map((day) => (
+            <Day
+              key={day}
+              text={day}
+              isActive={active === day}
+              handleClick={() => setActive(day)}
+            >
+              {day}
+            </Day>
+          ))}
         </div>
-    );
+
+        {/* Events of the selected day */}
+        {events[active] ? showEvents(events[active]) : showNone()}
+      </div>
+
+      {/* Medium and smaller screen */}
+      <div className="lg:hidden overflow-hidden">
+        {days.map((day, i) => (
+          <MobileDay
+            index={i + 1}
+            key={day}
+            text={day}
+            events={events[day] ? showEvents(events[day]) : showNone()}
+          />
+        ))}
+      </div>
+    </>
+  );
 }
