696a2027ebc8797571adea69e5eb7961f9eec0ad
Christian Bingman <42191425+ChristianBingman@users.noreply.github.com>
2023-02-15 17:44:12 -0600

    Add Sponsors Section and Fix Sponsors Squish (Remove Grid) (#144)
    Removed grid from sponsors section and added it to the home page.
    
    Resolves #132
    
    Co-authored-by: Christian Bingman <christian@christianbingman.com>

--- src/components/SponsorsSection/index.jsx
+++ src/components/SponsorsSection/index.jsx
@@ -4,16 +4,16 @@ import { IMAGE_LINKS } from "./allsponsors";
 export default function SponsorsSection(){
     const imgs = IMAGE_LINKS.map((image, idx) => {
         return (
-            <div key={idx} className="basis-1/5">
+            <div key={idx} className="">
                 <CircularImg key={idx} href={image}/>
             </div>
         )
     });
 
     return (
-        <div className='grid place-items-center'>
+        <div className='container mx-auto flex flex-col text-center justify-center'>
             <h1 className='my-4'>Sponsors</h1>
-            <div className='flex flex-row flex-wrap justify-center w-3/4'>
+            <div className='flex flex-row flex-wrap justify-center gap-2 place-content-evenly'>
                 {imgs}
             </div>
         </div>
