d00fdf929a72a0965ef9c148bd913d66fd7e9402
Marko Bogosavljevic <7422135+ThatMarko@users.noreply.github.com>
2023-02-15 20:06:37 -0600

    Add valid address for each dummy event.

--- src/api/DummyEvents/index.js
+++ src/api/DummyEvents/index.js
@@ -4,7 +4,11 @@ const DummyEvents = {
       id: 1,
       time: "12:00 PM",
       date: "01/01/2022",
-      address: "dummy address",
+      address: [
+        41.8706738, //lat
+        -87.6485639, //log
+        "UIC Science and Engineering Labs East Building", //Label (adress or search term)
+      ],
       header: "Git Workshop with Linus Torvalds",
       description:
         "Want to get better at git? Want to learn how you can contribute to open source? Learn from the greatest! Linus Torvalds! This workshop will cover forking repos, guidelines, pull requests, and why you should contribute! Food will be provided immediately after as well as mercy. Merchandise will also be available! This event will be hosted in the CS Lounge!",
@@ -13,7 +17,37 @@ const DummyEvents = {
       id: 2,
       time: "07:00 PM",
       date: "01/01/2022",
-      adress: "dummy adress",
+      address: [
+        41.87186432439297,
+        -87.64798028852418,
+        "UIC+Student+Center+East",
+      ],
+      header: "Build a PC with Linus Sebastian",
+      description:
+        "Let's build a modern, budget-friendly PC with Linus Sebastian from Linus Tech Tips. Learn how pcpartpicker.com can help you to manage and track the price of your components. Learn how to find the best deals and how to calculate when the prices will drop. The event will be hosted at SCE.",
+    },
+    {
+      id: 3,
+      time: "12:00 PM",
+      date: "01/01/2022",
+      address: [
+        41.8706738,
+        -87.6485639,
+        "UIC Science and Engineering Labs East Building",
+      ],
+      header: "Git Workshop with Linus Torvalds",
+      description:
+        "Want to get better at git? Want to learn how you can contribute to open source? Learn from the greatest! Linus Torvalds! This workshop will cover forking repos, guidelines, pull requests, and why you should contribute! Food will be provided immediately after as well as mercy. Merchandise will also be available! This event will be hosted in the CS Lounge!",
+    },
+    {
+      id: 4,
+      time: "07:00 PM",
+      date: "01/01/2022",
+      address: [
+        41.87186432439297,
+        -87.64798028852418,
+        "UIC+Student+Center+East",
+      ],
       header: "Build a PC with Linus Sebastian",
       description:
         "Let's build a modern, budget-friendly PC with Linus Sebastian from Linus Tech Tips. Learn how pcpartpicker.com can help you to manage and track the price of your components. Learn how to find the best deals and how to calculate when the prices will drop. The event will be hosted virtually via zoom.",
@@ -24,7 +58,11 @@ const DummyEvents = {
       id: 3,
       time: "05:00 PM",
       date: "01/03/2022",
-      adress: "dummy adress",
+      address: [
+        41.8706738,
+        -87.6485639,
+        "UIC Science and Engineering Labs East Building",
+      ],
       header: " Nvidia Drivers with Linus Torvalds",
       description:
         "Have you tried switching to Linux but you had issues installing Nvidia drivers? Join us and learn how Nvidia is making switching to Linux harder than necessary. Learn why using an AMD is significantly easier to manage with Linus Torvalds! This workshop will cover installing Nvidia drivers on Ubuntu and Fedora. Snacks will be provided. This event will be hosted in the CS Lounge!",
