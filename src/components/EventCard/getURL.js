ab457e9739d61dda10a3d151163eeb8ecca6eaa5
Marko Bogosavljevic <7422135+ThatMarko@users.noreply.github.com>
2023-02-15 20:10:48 -0600

    Generate link url based on platform.

---
+++ src/components/EventCard/getURL.js
@@ -0,0 +1,31 @@
+//Check if the user is on a mobile phone
+const isPhone = /Mobile/i.test(navigator.userAgent);
+
+//Check what the platform of user is
+const isAndroid = /Android/i.test(navigator.userAgent);
+const isIOS = /iPhone|iPad|iPod/i.test(navigator.userAgent);
+
+//Dynamically generate URLs
+export default function getURL(latitude, longitude, label) {
+  //Handle phones
+  if (isPhone) {
+    return isAndroid
+      ? "geo:" + latitude + "," + longitude + "?q=" + encodeURIComponent(label)
+      : "maps://" +
+          latitude +
+          "," +
+          longitude +
+          "?q=" +
+          encodeURIComponent(label);
+  }
+
+  //Handle desktop
+  return (
+    "https://www.google.com/maps/search/?api=1&query=" +
+    encodeURI(label) +
+    "&center=" +
+    latitude +
+    "%2C" +
+    longitude
+  );
+}
