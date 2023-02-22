8211624359ea7c41d7dd44c13ce4d4fb214ca2cb
Mike Bolloskis <79297389+Mpb0406@users.noreply.github.com>
2023-02-22 18:39:35 -0500

    Resolves issue #148 - Update Website Font (#152)
    * Resolves issue #148 - Update Website Font
    
    Added Montserrat font to index.html and updated tailwind theme with font
    
    * Issue #148 - Fixes font weight
    
    Updated font link in index.html to include 600 semibold and 700 bold

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    fontFamily: {
+      sans: ["Montserrat", "sans-serif"],
+    },
     colors: {
       // Defines primary, primary-light, primary-dark
       primary: {
