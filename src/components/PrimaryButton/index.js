cd1a4a40b83cc5b69d1ec217da95a6c6a028a21d
bitcode <marcdrozrosario@gmail.com>
2023-02-24 15:14:00 -0600

    Fix button width issue on Contact Us page (#161)

--- src/components/PrimaryButton/index.js
+++ src/components/PrimaryButton/index.js
@@ -2,10 +2,10 @@ export default function PrimaryButton(props) {
   return (
     <button
       className="rounded-[40px] bg-primary-light text-white font-bold hover:bg-primary-dark py-2 px-6 w-fit"
-      style={{ maxHeight: "75px" }}
-      onClick={props.onClick}
+      style={ { maxHeight: "75px", maxWidth: "none", minWidth: "100%" } }
+      onClick={ props.onClick }
     >
-      <h3>{props.innerText}</h3>
+      <h3>{ props.innerText }</h3>
     </button>
   );
 }
