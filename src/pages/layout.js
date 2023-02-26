bc5b80c7399cbf7f1cb92758699a82e8e077e3d5
Adrian Knight <aknig2@uic.edu>
2023-02-26 00:21:32 -0600

    Sticky header created. Shifting the page content below header is done by encompassing divs height

--- src/pages/layout.js
+++ src/pages/layout.js
@@ -4,13 +4,15 @@ import { Link, Outlet } from 'react-router-dom'
 export default function Home(){
     return (
         <div>
-            <nav>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/about">About</Link></li>
-                    <li><Link to="/eventdetails">Event Details</Link></li>
-                    <li><Link to="/contact">Contact</Link></li>
-                    <li><Link to="/social">Social</Link></li>
+            <div className={"h-16 mb-1"}></div>
+            <nav className={"fixed block top-0 w-full py-7 bg-[#3E3B87] z-50"}>
+                <ul className={""}>
+                    <li className={"w-fit m-0 inline-block pl-4 pr-2"}><h5><b>SparkHacks 2023</b></h5></li>
+                    <li className={"w-fit m-0 inline-block px-2"}><Link to="/"><b>Home</b></Link></li>
+                    <li className={"w-fit m-0 inline-block px-2"}><Link to="/contact"><b>RSVP</b></Link></li>
+                    <li className={"w-fit m-0 inline-block px-2"}><Link to="/social"><b>Social</b></Link></li>
+                    <li className={"w-fit m-0 inline-block px-2"}><Link to="/eventdetails"><b>Calendar</b></Link></li>
+                    <li className={"w-fit m-0 inline-block px-2"}><Link to="/about"><b>About</b></Link></li>
                 </ul>
             </nav>
             <br />
