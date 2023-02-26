946a153f58b045203e78db8c2d55f223809b9c08
ismetkync <ismetkync737@gmail.com>
2023-02-26 12:28:20 +0300

    Create Website Footer

--- src/pages/layout.js
+++ src/pages/layout.js
@@ -1,9 +1,9 @@
 import './layout.css'
 import { Link, Outlet } from 'react-router-dom'
 
-export default function Home(){
+export default function Home() {
     return (
-        <div>
+        <div className='flex flex-col justify-between h-screen'>
             <nav>
                 <ul>
                     <li><Link to="/">Home</Link></li>
@@ -14,8 +14,37 @@ export default function Home(){
                 </ul>
             </nav>
             <br />
-            
+
             <Outlet />
+
+            <div className='bg-[#000] p-2'>
+                <div className='flex flex-wrap p-2 '>
+                    <div className=' text-center w-1/2'>
+                        <h4>Pages</h4>
+                        <ul>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="/contact">RSVP</Link></li>
+                            <li><Link to="/social">Social</Link></li>
+                            <li><Link to="/eventdetails">Calendar</Link></li>
+                            <li><Link to="/about">About</Link></li>
+                        </ul>
+                    </div>
+                    <div className='text-center w-1/2'>
+                        <h4>Social</h4>
+                        <ul>
+                            <li><Link to="/">Instagram</Link></li>
+                            <li><Link to="/about">Facebook</Link></li>
+                            <li><Link to="/about">Youtube</Link></li>
+                            <li><Link to="/about">Twitter</Link></li>
+                            <li><Link to="/about">LinkedIn</Link></li>
+                        </ul>
+                    </div>
+                </div>
+                <div className='text-center'>
+                    <p className='text-[12px] '>Copyright © Association for Computing Machinery 2023</p>
+                    <p className='text-[12px]'>License is located on this sites Github Repo.</p>
+                </div>
+            </div>
         </div>
     )
 }
