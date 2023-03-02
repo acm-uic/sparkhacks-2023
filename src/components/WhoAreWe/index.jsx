f218b6b169dfe613e980f15713606cc5f061979d
Christian Bingman <42191425+ChristianBingman@users.noreply.github.com>
2023-03-01 21:18:08 -0600

    Added home page content and updated description and who are we. (#178)
    Co-authored-by: Christian Bingman <christian@christianbingman.com>

--- src/components/WhoAreWe/index.jsx
+++ src/components/WhoAreWe/index.jsx
@@ -6,7 +6,7 @@ export default function WhoAreWe(){
                     <h1>Who We Are</h1>
                 </div>
                 <div className={"sm:ml-[10rem] min-w-fit"}>
-                    <p>The Flourish! Council is a small group of ~10 students who are interested in open source technologies and want to bring this knowledge to other students! We are all UIC students ranging from 1st year to graduate with varying levels of experience. All with a common goal in of teaching about open source! Our SIG WebDev created this site as our first official project aimed at increasing interest through a fun and informative website! See below for more information about the executive project leaders and our amazing speakers!</p>
+                    <p>SparkHacks is a collegiate hackathon intended to give individuals an opportunity to learn and join their knowledge to create a solution towards a common goal. We are a group of student organizations at UIC working together to build an inclusive environment for students to work on their projects. We are mainly focused on Computer Science which is where our passion lies as well as building a community at UIC! We are made up of ACM, LUG, WiCS, and SWE. This website was made by a Special Interest Group of ACM which develops websites, and SIG SysAdmin which provides the hosting platform for this site! WiCS and SWE encourage women and other underrepresented groups to pursue careers in tech and to support these groups throughout their university careers. LUG is a student organization that focuses on bringing more knowledge of computing with the Linux operating system and provides support for the backbone of tech stacks.</p>
                 </div>
             </div>
             
---
+++ src/content/StaggeredTextComponents.js
@@ -0,0 +1,94 @@
+ export const WhatDoIGetOutOfThis = [
+        {
+            id: 1,
+            header: "Project Experience",
+            body: "As part of SparkHacks, you will get experience working on something that YOU 🫵 create! No upfront experience is required, and working with a small team of people will help you gain experience with topics that you may not get to learn in a university setting. You can also add the project to your resume for future job opportunities, and your project will be visible to our corporate sponsors after the event!"
+        },
+        {
+            id: 2,
+            header: "Valuable Skills",
+            body: "Workshops given by our hackathon staff, alumni, and sponsors will cover important technical and non technical topics for you to be able to gain experience in tools and technologies you may have never interfaced with before!"
+        },
+        {
+            id: 3,
+            header: "Teamwork",
+            body: "Whether you join with a team of 4 other people, or choose to be placed on a random team, you will dive deep into a specific topic and collaborate with others to put together a great project in the end!"
+        },
+        {
+            id: 4,
+            header: "Resume Boost",
+            body: "By the end of SparkHacks, you will gain a valuable project that you can put on your resume that demonstrates your skills, new and old, as well as your ability to work with a team, which employers love to see!"
+        },
+        {
+            id: 5,
+            header: "Create a Sense of Community",
+            body: "Along with working on a project, you will get the opportunity to meet new people in the social breaks and during meals. You can gain valuable networking experience with alumni, staff, professors, sponsors, and employers"
+        },
+    ]
+export const FAQs = [
+  {
+    id: 0,
+    header: "How do I participate?",
+    body: "You will need to fill out our RSVP form when it comes out, and it is recommended that you join our mailing list to get important information as it comes out!"
+  },
+  {
+    id: 1,
+    header: "How can I stay updated with the event?",
+    body: "Join our newsletter! You will get important updates about the event as they come, and you will be the first to know!"
+  },
+  {
+    id: 2,
+    header: "Who is allowed to attend?",
+    body: "All UIC students are allowed to participate in the event. The event will have options for no code entries, and teams can be made up of code and no code participants!"
+  },
+  {
+    id: 3,
+    header: "When and where will the event be located?",
+    body: "Join us in-person on April 7th to April 8th at the Student Center East! You can click on events to see where they are located once they are available!"
+  },
+  {
+    id: 4,
+    header: "Do I need to have a programming background to participate?",
+    body: "Nope! No programming background of any level is required, but an interest in CS is recommended since the projects will be CS focused."
+  },
+  {
+    id: 5,
+    header: "What should I bring?",
+    body: "You will be working on a project which will require some amount of interaction with a computer, so a personal computer will be required along with your student ID and your laptop charger. It is also recommended that you bring a water bottle. It is not recommended, however, to bring large electronics 🚫"
+  },
+  {
+    id: 6,
+    header: "Is there any to do other than code?",
+    body: "There will be no code options along with things to learn and food!"
+  },
+  {
+    id: 7,
+    header: "Will there be food?",
+    body: "Yes!"
+  },
+  {
+    id: 8,
+    header: "Will it be free?",
+    body: "Yes! The entire event is free for UIC students."
+  },
+  {
+    id: 9,
+    header: "Do I need a team? How do I find one?",
+    body: "There will be an intro meet and greet session where you can talk to people and make your teams! You should have a team in order to ensure you get the most value out of this event. If you are having a hard time finding a team, please reach out!"
+  },
+  {
+    id: 10,
+    header: "Do I have to go to the opening/closing ceremony?",
+    body: "It is highly recommended as that will be your opportunity to see the projects people have been working on, see the winners, and learn important details about your theme and project!"
+  },
+  {
+    id: 11,
+    header: "What if I have class on Friday?",
+    body: "You will make the information that you need available to you once the workshop or ceremony has completed!"
+  },
+  {
+    id: 12,
+    header: "Will I be required to be in person?",
+    body: "This is an in person ONLY event. We have sponsors who want to see you work with other people, and you will miss out on the primary benefits of attending the event. So make plans!"
+  },
+]
