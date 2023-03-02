d8fcfaf2be4f1c52f3355ea363408386666312ce
Ajknight121 <aknig2@uic.edu>
2023-03-02 11:06:41 -0600

    Created fading animations and added them to Header (#177)
    * Created fading animations and added them to Header
    
    * Extended animation to navbar
    
    Also added light background color to the spacer div

--- src/components/HomeHeader/index.js
+++ src/components/HomeHeader/index.js
@@ -3,9 +3,9 @@ import PrimaryButton from '../PrimaryButton'
 
 export default function HomeHeader(){
   return (
-    <div className="w-full h-screen bg-primary-light relative flex text-center items-center justify-center">
-      <img src={sparklogo} className="opacity-50 h-2/3"/>
-      <div className="absolute container">
+    <div className="w-full h-screen bg-primary-light relative flex text-center items-center justify-center ">
+      <img src={sparklogo} alt={"Sparkhacks logo"} className="opacity-50 h-2/3 animate-fade-in-slow-fade-out"/>
+      <div className="absolute container delay-1000 animate-fade-in">
         <h1 className="md:text-8xl">SparkHacks 2023</h1>
         <h2>April 7-8th</h2>
         <div className="flex items-center justify-center flex-wrap md:flex-nowrap my-5 gap-5 mx-5">
--- src/pages/layout.js
+++ src/pages/layout.js
@@ -5,8 +5,8 @@ import sparkLogo from "../assets/images/sparkHacksLogo.png"
 export default function Home(){
     return (
         <div>
-            <div className={"h-5 mb-1"}></div>
-            <nav className={"fixed block top-0 w-full bg-primary-light z-50"}>
+            <div className={"h-14 pb-1 bg-primary-light"}></div>
+            <nav className={"fixed block top-0 w-full bg-primary-light z-50 animate-fade-in"}>
                 <ul className={"flex items-center"}>
                     <li ><img src={sparkLogo} className={"h-12 m-0 inline-block px-1 py-1 align-center box-content"} alt={"spark hacks logo"}/></li>
                     <li className={"w-fit h-fit m-0 inline-block pl-4 pr-2 align-center"}><h5><b>SparkHacks 2023</b></h5></li>
@@ -17,7 +17,6 @@ export default function Home(){
                     <li className={"w-fit h-fit m-0 inline-block px-2"}><Link to="/about"><b>About</b></Link></li>
                 </ul>
             </nav>
-            <br />
             
             <Outlet />
         </div>
