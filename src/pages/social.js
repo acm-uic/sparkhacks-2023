e15a1f345bdfd4dab6765414cc06f5707050723c
Christian Bingman <christian@christianbingman.com>
2023-03-03 21:46:31 -0600

    Add Mobile Newsletter and Fix Footer
    Resolves #81

--- src/pages/social.js
+++ src/pages/social.js
@@ -2,15 +2,17 @@ import "./social.css";
 import DesktopSMBubbles from "../components/DesktopSMBubbles";
 import DesktopNewsletter from "../components/DesktopNewsletter";
 import MobileSMBubbles from "../components/MobileSMBubbles";
+import MobileNewsletter from "../components/MobileNewsletter"
 
 export default function Social() {
   return (
     <div>
       <div className="hidden lg:block">
-        <DesktopSMBubbles />
         <DesktopNewsletter />
+        <DesktopSMBubbles />
       </div>
       <div className="lg:hidden">
+        <MobileNewsletter />
         <MobileSMBubbles />
       </div>
     </div>
