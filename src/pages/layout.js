7f7321724e512ff483e7ec1e80663dc582b153cb
İsmet Ömer Koyuncu <79707353+iomerkoyuncu@users.noreply.github.com>
2023-03-04 06:23:14 +0300

    Create Website Footer (#168)
    * Create Website Footer
    
    * added-link
    
    * Added Github Link
    
    * Create Website Footer
    
    ---------
    
    Co-authored-by: Christian Bingman <42191425+ChristianBingman@users.noreply.github.com>

--- src/pages/layout.js
+++ src/pages/layout.js
@@ -31,8 +31,37 @@ export default function Home(){
           <li><Link onClick={() => setCurrentPage("About")} className={currentPage == "About" ? "font-bold": ""} to="/about">About</Link></li>
         </ul>
       </nav>
-
       <Outlet />
+      <div className='bg-[#000] p-2'>
+                <div className='flex flex-wrap p-2 '>
+                    <div className=' text-center w-1/2'>
+                        <h4>Pages</h4>
+                        <ul>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="/contact">RSVP</Link></li>
+                            <li><Link to="/social">Social</Link></li>
+                            <li><Link to="/eventdetails">Calendar</Link></li>
+                            <li><Link to="/about">About</Link></li>
+                        </ul>
+                    </div>
+                    <div className='text-center w-1/2'>
+                        <h4>Social</h4>
+                        <ul>
+                            <li><Link to="/">Instagram</Link></li>
+                            <li><Link to="/about">Facebook</Link></li>
+                            <li><Link to="/about">Youtube</Link></li>
+                            <li><Link to="/about">Twitter</Link></li>
+                            <li><Link to="/about">LinkedIn</Link></li>
+                        </ul>
+                    </div>
+                </div>
+                <div className='text-center'>
+                    <p className='text-[12px]'>Copyright © Association for Computing Machinery 2023</p>
+                    <p className='text-[12px]'>License is located on this sites
+                        <a href="https://github.com/acm-uic/sparkhacks-2023">Github Repo.</a>
+                    </p>
+                </div>
+            </div>
     </div>
   )
 }
