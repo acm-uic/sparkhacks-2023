fdf1ace30605f9b8b86c6ff6d43795e2c05517a1
Abhishek Lingwal <93655183+Abhishek7Tech@users.noreply.github.com>
2023-03-14 07:44:13 +0530

    Fix speaker section imgs (#196)
    * fixed
    
    * fixed

--- src/components/SpeakersSection/index.jsx
+++ src/components/SpeakersSection/index.jsx
@@ -1,19 +1,17 @@
 import CircularImg from "../CircularImg";
 import { IMAGE_LINKS } from "./allspeakers";
 
-export default function SpeakersSection(){
-    const imgs = IMAGE_LINKS.map((image, idx) => {
-        return (
-            <CircularImg key={idx} href={image}/>
-        )
-    });
+export default function SpeakersSection() {
+  const imgs = IMAGE_LINKS.map((image, idx) => {
+    return <CircularImg key={idx} href={image} />;
+  });
 
-    return (
-        <div className='grid place-items-center'>
-            <h1 className='my-4'>Speakers</h1>
-            <div className='grid grid-cols-1 gap-3 md:grid-cols-3 lg:grid-cols-5 m-2'>
-            {imgs}
-            </div>
-        </div>
-    );
+  return (
+    <div className="grid place-items-center">
+      <h1 className="my-4">Speakers</h1>
+      <div className="flex flex-col gap-4 justify-center md:flex-row flex-wrap">
+        {imgs}
+      </div>
+    </div>
+  );
 }
