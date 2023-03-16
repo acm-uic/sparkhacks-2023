26cfa1de976647c112744c153785a21306cc38e4
martinyis <martinyis11@gmail.com>
2023-03-16 16:34:15 -0400

    Reordered components and reduced margin

--- src/components/DesktopSMBubbles/index.jsx
+++ src/components/DesktopSMBubbles/index.jsx
@@ -1,21 +1,27 @@
-import { FacebookSMBubble, InstagramSMBubble, LinkedinSMBubble, TwitterSMBubble, YoutubeSMBubble } from "../SocialMediaBubbles";
+import {
+  FacebookSMBubble,
+  InstagramSMBubble,
+  LinkedinSMBubble,
+  TwitterSMBubble,
+  YoutubeSMBubble,
+} from "../SocialMediaBubbles";
 
 export default function DesktopSMBubbles() {
-    return( 
-        <div className="grid h-screen place-items-center">  
-            <div className="relative items-center justify-center inline-flex w-7/12" >
-                <div className="relative items-center justify-center inline-flex z-0">
-                    <InstagramSMBubble/>
-                        <div className="relative items-center justify-center inline-flex w-5/12 z-10">
-                            <LinkedinSMBubble/>
-                            <div className="relative items-center justify-center inline-flex w-3/12 z-20">
-                                <YoutubeSMBubble/>
-                            </div>
-                            <TwitterSMBubble/>
-                        </div>
-                    <FacebookSMBubble/>
-                </div>
+  return (
+    <div className="grid h-screen place-items-center py-5">
+      <div className="relative items-center justify-center inline-flex w-7/12">
+        <div className="relative items-center justify-center inline-flex z-0 py-5">
+          <InstagramSMBubble />
+          <div className="relative items-center justify-center inline-flex w-5/12 z-10">
+            <LinkedinSMBubble />
+            <div className="relative items-center justify-center inline-flex w-3/12 z-20">
+              <YoutubeSMBubble />
             </div>
+            <TwitterSMBubble />
+          </div>
+          <FacebookSMBubble />
         </div>
-    );
+      </div>
+    </div>
+  );
 }
--- src/pages/social.js
+++ src/pages/social.js
@@ -2,18 +2,18 @@ import "./social.css";
 import DesktopSMBubbles from "../components/DesktopSMBubbles";
 import DesktopNewsletter from "../components/DesktopNewsletter";
 import MobileSMBubbles from "../components/MobileSMBubbles";
-import MobileNewsletter from "../components/MobileNewsletter"
+import MobileNewsletter from "../components/MobileNewsletter";
 
 export default function Social() {
   return (
     <div>
       <div className="hidden lg:block">
-        <DesktopNewsletter />
         <DesktopSMBubbles />
+        <DesktopNewsletter />
       </div>
       <div className="lg:hidden">
-        <MobileNewsletter />
         <MobileSMBubbles />
+        <MobileNewsletter />
       </div>
     </div>
   );
