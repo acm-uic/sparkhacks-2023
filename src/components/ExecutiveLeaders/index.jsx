ee6b8f6849539fcd7db3378af90b9d7744c9506a
Christian Bingman <christian@christianbingman.com>
2023-03-21 21:58:28 -0500

    Updated Sponsors, removed extra sections, cleanup

--- src/components/ExecutiveLeaders/index.jsx
+++ src/components/ExecutiveLeaders/index.jsx
@@ -2,18 +2,16 @@ import CircularImg from "../CircularImg";
 import { IMAGE_LINKS } from "./allexecutives";
 
 export default function ExecutiveLeaders(){
-    const imgs = IMAGE_LINKS.map((image, idx) => {
-        return (
-            <CircularImg key={idx} href={image} />
-        )
-    })
+  const imgs = IMAGE_LINKS.map((image, idx) => {
+    return <CircularImg key={idx} href={image} />;
+  });
 
-    return (
-        <div className='grid place-items-center'>
-            <h1 className='flex flex-col text-center my-4'>Executive Leaders</h1>
-            <div className='grid grid-cols-1 gap-3 md:grid-cols-3 lg:grid-cols-3 m-2'>
-                {imgs}
-            </div>
-        </div>
-    );
+  return (
+    <div className="grid place-items-center">
+      <h1 className="my-4">Executive Leaders</h1>
+      <div className="flex flex-col gap-4 justify-center md:flex-row flex-wrap">
+        {imgs}
+      </div>
+    </div>
+  );
 }
--- src/components/SponsorsSection/allsponsors.jsx
+++ src/components/SponsorsSection/allsponsors.jsx
@@ -1,3 +1,13 @@
+import Salesforce from '../../assets/images/salesforce.png'
+import Cat from '../../assets/images/cat.png'
+import Abbvie from '../../assets/images/abbvie.jpg'
+import Grainger from '../../assets/images/grainger.png'
+import Codepath from '../../assets/images/codepath.png'
 export const IMAGE_LINKS = [
     "https://pbs.twimg.com/profile_images/999836329230843904/8H5CIIXK_400x400.jpg",
+    Salesforce,
+    Cat,
+    Abbvie,
+    Grainger,
+    Codepath,
 ];
