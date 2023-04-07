cec0c8afba9280717f6ee5d24d40f6325db856ef
Christian Bingman <christian@christianbingman.com>
2023-04-06 20:11:19 -0500

    Updated events, locations, and added events to home

--- src/api/DummyEvents/index.js
+++ src/api/DummyEvents/index.js
@@ -10,7 +10,7 @@ const Events = {
         "UIC+Student+Center+East",
       ],
       header: "Check In",
-      description: "Check in to SparkHacks in SCE in front of the Illinois room and mingle with other attendees!"
+      description: "Check in to sparkhacks at SCE in front of Illinois room & mingle with fellow attendees - decide on a team if not already done - Join us early on Friday for a fun Ice Breaker Activity to get to know other students before the Opening Ceremony! You will get a chance to interact with other students, mentos, sponsors, giving you the perfect opportunity to form teams prior to the hackathon. Remember, teams can be up to 4 members."
     },
     {
       id: 2,
@@ -22,7 +22,7 @@ const Events = {
         "UIC+Student+Center+East",
       ],
       header: "Opening Remarks",
-      description: "UIC's first collegiate CS hackathon kick off ceremony! We will introduce the prompts and categories in the Illinois room. Illinois Room AB"
+      description: "Join us to kick off our 1st student run hackathon at UIC, take place in Illinois room we will introduce the projects and the theme of SparkHacks 2023! Illinois Room AB"
     },
     {
       id: 3,
@@ -34,7 +34,7 @@ const Events = {
         "UIC+Student+Center+East",
       ],
       header: "Introduction to GitHub and Git",
-      description: "Workshop with Christian Bingman and Krish Bavana that will cover an introduction to working on a team using GitHub. You will set up your code repository and work with your teammates to start organizing your work! Illinois Room AB"
+      description: "Explore Version Control and how to use GitHub for Project Management. This is a golden opportunity to learn new technical skills, crucial to the industry and collaborating with others. Please make sure you download GitHub Desktop beforehand from here : https://desktop.github.com/ and create your GitHub account! Illinois Room AB"
     },
     {
       id: 4,
@@ -46,7 +46,7 @@ const Events = {
         "UIC+Student+Center+East",
       ],
       header: "Creating Interactive Models Using Figma",
-      description: "Presented by Chris from Swift, learn how to create designs and models using the popular design tool Figma. Illinois Room AB"
+      description: "Interested in making fun UIs with minimal code options? This workshop is the perfect way for students to learn more about front end development.  If you are interested in this workshop please make sure to create an account on https://www.figma.com/ beforehand. Illinois Room AB"
     },
     {
       id: 5,
@@ -57,32 +57,32 @@ const Events = {
         -87.64798028852418,
         "UIC+Student+Center+East",
       ],
-      header: "Continuous Deployment of Cloud Native Applications by Grainger",
-      description: "Special topic presented by Grainger. Cardinal Room"
+      header: "Continuous Deployment of Cloud Native Applications",
+      description: "Learn how applications are deployed and hosted on servers. This workshop will help you understand the importance of cloud and how easy it is to host an application. Cardinal Room"
     },
     {
       id: 6,
       time: "6:00 PM",
       date: "04/07/2023",
       address: [
-        41.87186432439297,
-        -87.64798028852418,
-        "UIC+Student+Center+East",
+        41.872103,
+        -87.648725,
+        "Lecture+Center+C",
       ],
       header: "Introduction to App Development with React Native",
-      description: "Presented by the Leaf Project. Learn how to build apps for IOS and Android using React Native. Illinois Room C"
+      description: "Join us for a chance to learn about the ins and outs of using React Native and Introduce programmers to the types of technology used to construct applications for hand-held devices.  You would need to have an account at https://expo.dev/ prior to the event. Lecture Hall C1"
     },
     {
       id: 7,
       time: "6:00 PM",
       date: "04/07/2023",
       address: [
-        41.87186432439297,
-        -87.64798028852418,
-        "UIC+Student+Center+East",
+        41.872103,
+        -87.648725,
+        "Lecture+Center+C",
       ],
       header: "Introduction to Web Development with React",
-      description: "Presented by Chase Lee. Learn to build modern web applications using React. Cardinal Room"
+      description: "Learn how to use React, NodeJS, HTML and CSS to build a website. You get a chance to explore frontend development using React as well as to build simple web components and applications. Lecture Hall C3"
     },
     {
       id: 8,
@@ -94,7 +94,7 @@ const Events = {
         "UIC+Student+Center+East",
       ],
       header: "Dinner Social",
-      description: "Presentation by NSBE over Dinner. Illinois Room AB"
+      description: "Join NSBE along with other hackers in some time to destress with a game of Jeopardy! We will also be having other board games that you can play with your fellow team members and face off against other teams. Illinois Room AB and Cardinal Room"
     },
     {
       id: 9,
@@ -120,7 +120,7 @@ const Events = {
         "UIC+Student+Center+East",
       ],
       header: "Breakfast and Work Time",
-      description: "Check in for Day 2 of the event. Breakfast will be provided free of charge, feel free to take this time to network with other students and work on your projects! Inner Circle"
+      description: "Check in to sparkhacks at SCE in front of Illinois room & mingle with fellow attendees. Inner Circle"
     },
     {
       id: 2,
@@ -136,15 +136,15 @@ const Events = {
     },
     {
       id: 3,
-      time: "9:05 AM",
+      time: "10:00 AM",
       date: "04/08/2023",
       address: [
-        41.87186432439297,
-        -87.64798028852418,
-        "UIC+Student+Center+East",
+        41.872103,
+        -87.648725,
+        "Lecture+Center+C",
       ],
-      header: "Scavenger Hunt",
-      description: "UIC Scavenger Hunt! Details to come! Inner Circle"
+      header: "Introduction to Machine Learning",
+      description: "This workshop aims to give you an introduction to machine learning via Decision Trees & Neural Networks.  You will also leave this workshop learning how to work with an open source chat gpt model! Lecture Hall C1"
     },
     {
       id: 4,
@@ -155,20 +155,20 @@ const Events = {
         -87.64798028852418,
         "UIC+Student+Center+East",
       ],
-      header: "Introduction to Machine Learning",
-      description: "A brief introduction into machine learning with J Jesus. Learn the fundamental concepts behind what makes the popular AI bots work! Cardinal Room"
+      header: "Strategies for Effective Interviewing (AbbVie)",
+      description: "This workshop will prepare you to effectively navigate the interview process. We will cover strategies you can put in place to prepare for an interview, effectively tell your story during the interview, and follow-up after your interview. This will be an interactive session, allowing you to practice answering commonly asked interview questions. Cardinal Room"
     },
     {
       id: 5,
       time: "11:00 AM",
       date: "04/08/2023",
       address: [
-        41.87186432439297,
-        -87.64798028852418,
-        "UIC+Student+Center+East",
+        41.872103,
+        -87.648725,
+        "Lecture+Center+C",
       ],
       header: "Introduction to Web App Penetration Testing Workshop",
-      description: "Presented by José Martinez & Sydney from Accenture! Learn the basics of security vulnerability testing on web based applications and services."
+      description: "This workshop will introduce you to the world of penetration testing. You will learn how to set up a pentesting environment and how to do some simple exploits! Web penetration testing is a process of identifying and exploiting vulnerabilities in web applications. It is an essential component of security testing, as web applications are often the primary target for attackers. We require that you have the following software installed through this Set Up Guide :  https://tinyurl.com/4vsn79a8. Lecture Hall C1"
     },
     {
       id: 6,
@@ -180,7 +180,7 @@ const Events = {
         "UIC+Student+Center+East",
       ],
       header: "Monoliths and Microservices",
-      description: "Presented by Grainger. Learn about the differences between monolith and micro services when developing large scale applications."
+      description: "Microservices and Monoliths are two different architectural approaches for designing and developing software applications. This workshop helps you to understand how to design your systems with better performances. Monoliths focuses on running the solution as a single-contained application, whereas microservices focuses on breaking the large solution into smaller solutions. Cardinal Room"
     },
     {
       id: 7,
@@ -204,10 +204,22 @@ const Events = {
         "UIC+Student+Center+East",
       ],
       header: "Technical Interview Preparation and Bahavioral Interviewing",
-      description: "Learn how to effectively prepare for technical and behavioral interviews with Codepath!"
+      description: "Join us for practical tips and strategies to prepare for and excel in behavioral interviews, led by an experienced CodePath career coach. Designed to help you land your dream job in tech, this workshop will provide you with the tools you need to showcase your skills and stand out to potential employers. Cardinal Room"
     },
     {
       id: 9,
+      time: "4:30 PM",
+      date: "04/08/2023",
+      address: [
+        41.87186432439297,
+        -87.64798028852418,
+        "UIC+Student+Center+East",
+      ],
+      header: "Scavenger Hunt",
+      description: "What better way to bond with your teammates than running around in circles searching for clues. This Scavenger Hunt would be the perfect way to take a break from your projects and hacking to run around and explore SCE! Cardinal Room"
+    },
+    {
+      id: 10,
       time: "4:30 PM",
       date: "04/08/2023",
       address: [
@@ -219,7 +231,7 @@ const Events = {
       description: "Time to submit your projects! You will submit your projects through dev hub into your projects category. More information will be provided before the deadline."
     },
     {
-      id: 10,
+      id: 11,
       time: "4:30 PM",
       date: "04/08/2023",
       address: [
@@ -228,10 +240,10 @@ const Events = {
         "UIC+Student+Center+East",
       ],
       header: "Project Expo",
-      description: "Demo your projects to other students! Take some time to show off your work while the judges are looking over your projects!"
+      description: "After projects have been submitted successfully, you get to present your project to other participants, judges as well as sponsors through this expo structured like a Science Fair. Get a chance to present your final results through this Expo! East Terrace"
     },
     {
-      id: 11,
+      id: 12,
       time: "5:15 PM",
       date: "04/08/2023",
       address: [
@@ -243,7 +255,7 @@ const Events = {
       description: "Learn about the student organizations who made this possible! Short presentations from each of our member organizations! Dinner will be served during this event!"
     },
     {
-      id: 12,
+      id: 13,
       time: "6:00 PM",
       date: "04/08/2023",
       address: [
--- src/pages/home.js
+++ src/pages/home.js
@@ -4,11 +4,14 @@ import HomeHeader from '../components/HomeHeader/'
 import StaggeredSections from '../components/StaggeredSections/staggeredSections.js'
 import {WhatDoIGetOutOfThis, FAQs} from '../content/StaggeredTextComponents.js'
 import MemberOrganizationSection from '../components/MemberOrganizationsSection'
+import Events from "../components/Events";
+import DummyEvents from "../api/DummyEvents";
 
 export default function Home(){
-    return (<div className='text-center space-y-10'>
+    return (<div className='text-center'>
         <HomeHeader />
-        <div className='container mx-auto'>
+        <div className='container mx-auto space-y-10'>
+          <Events events={DummyEvents} />
           <h1>What Do I Get Out of This?</h1>
           <StaggeredSections sections={WhatDoIGetOutOfThis} />
           <SponsorsSection />
