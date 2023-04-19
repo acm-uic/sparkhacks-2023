b7c00579c936dc7dec406654367c8cb341acdfbf
Goutham <gouthamkumarpolapally@gmail.com>
2023-04-19 11:38:30 +0530

    Disabled the joining in news letter groups with beautiful alert component..

--- src/components/ContactUs/index.jsx
+++ src/components/ContactUs/index.jsx
@@ -1,11 +1,23 @@
 import PrimaryButton from "../PrimaryButton";
-import React from "react";
+import React,{useState} from "react";
 
 export default function ContactUs(){
     const [value, setValue] = React.useState('');
+    const[reason,setReason] = useState('');
+    const[details,setDetails] = useState('');
     const handleChange = (event) => {
         setValue(event.target.value);
+        setReason(event.target.value);
     };
+    const openEmail = () =>{
+        console.log(reason);
+        console.log(details);
+        const to = "info@sparkhacksatuic.com";
+        const subject = reason;
+        const body = details;
+        const emailLink = `mailto:${to}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+        return window.location.href = emailLink;
+    }
     return (
         <div className={"flex justify-center items-center min-h-[500px] sm:h-screen sm:bg-primary mt-5"}>
             <div className="flex-auto absolute text-center h-fit w-full min-w-max px-2 py-10 sm:w-1/2 sm:drop-shadow-[-90px_-60px_4px_rgba(0,0,0,0.5)] sm:bg-secondary-light sm:rounded-[2.813rem]">
@@ -22,8 +34,13 @@ export default function ContactUs(){
                         </select>
                     </div>
 
-                    <textarea type="textarea" rows={5} placeholder="Additional Details" />
-                    <PrimaryButton className={"w-full"} innerText="Send"/>
+                    <textarea type="textarea" rows={5} placeholder="Additional Details" 
+                        onChange = {(e)=>{setDetails(e.target.value)}}
+                    />
+                    <PrimaryButton className={"w-full"} innerText="Send"
+                        onClick = {openEmail}
+                        type="submit"
+                    />
                 </div>
             </div>
         </div>
