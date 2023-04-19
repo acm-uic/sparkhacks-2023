299055fcbe043f8969b5998875da4554aa39e333
Marko Bogosavljevic <7422135+ThatMarko@users.noreply.github.com>
2023-04-19 12:54:38 -0500

    Create a central file that contains all the links.

---
+++ src/api/SocialMediaLinks/index.js
@@ -0,0 +1,9 @@
+const SocialLinks = {
+    "LinkedIn": "https://www.linkedin.com/in/sparkhacks-uic-343612269/",
+    "Facebook": "https://www.facebook.com/profile.php?id=100090984219952",
+    "Twitter": "https://twitter.com/SparkhacksAtUIC",
+    "Instagram": "https://www.instagram.com/sparkhacksuic/",
+    "YouTube": "https://www.youtube.com/channel/UC9CxYBwFup_SCKp38-CQx8Q"
+}
+
+export default SocialLinks
