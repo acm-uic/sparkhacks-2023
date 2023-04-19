8cc385afbaac7470b4942f96c193019603d3e38a
Adrian Knight <aknig2@uic.edu>
2023-04-19 14:13:32 -0500

    Replaced date with closed event text

--- src/components/HomeHeader/index.js
+++ src/components/HomeHeader/index.js
@@ -9,7 +9,7 @@ export default function HomeHeader(){
       <img src={sparklogo} alt={"Sparkhacks logo"} className="opacity-50 h-2/3 animate-fade-in-slow-fade-out"/>
       <div className="absolute container delay-1000 animate-fade-in">
         <h1 className="md:text-8xl">SparkHacks 2023</h1>
-        <h2>April 7-8th</h2>
+        <h2>This event has concluded for this year! Feel free to contact us for questions.<br/> See you next year!</h2>
         <div className="flex items-center justify-center flex-wrap md:flex-nowrap my-5 gap-5 mx-5">
           <a className="flex-1" href='https://bit.ly/sparkhacks-registration-2023'><PrimaryButton innerText="RSVP" color={"dark"}/></a>
           <a className='flex-1' onClick={() => navigate('/social')} href='#'><PrimaryButton innerText="Newsletter" color={"dark"}/></a>
