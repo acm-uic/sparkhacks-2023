732b7cb517498218eb4c3aca3dfc8748cd2fb871
Ajknight121 <aknig2@uic.edu>
2023-04-19 16:44:52 -0500

    Replaced date with closed event text (#220)

--- src/components/HomeHeader/index.js
+++ src/components/HomeHeader/index.js
@@ -7,7 +7,7 @@ export default function HomeHeader(){
       <img src={sparklogo} alt={"Sparkhacks logo"} className="opacity-50 h-2/3 animate-fade-in-slow-fade-out"/>
       <div className="absolute container delay-1000 animate-fade-in">
         <h1 className="md:text-8xl">SparkHacks 2023</h1>
-        <h2>April 7-8th</h2>
+        <h2>This event has concluded for this year! Feel free to contact us for questions.<br/> See you next year!</h2>
         <div className="flex items-center justify-center flex-wrap md:flex-nowrap my-5 gap-5 mx-5">
           <a className="flex-1" aria-disabled={"true"}><PrimaryButton innerText="RSVP" color={"disable"}/></a>
           <a className='flex-1' aria-disabled={"true"}><PrimaryButton innerText="Newsletter" color={"disable"}/></a>
