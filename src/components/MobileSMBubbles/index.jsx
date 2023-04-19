03f748921330f32d1d4dedf50e1817d9b6241c5d
Marko Bogosavljevic <7422135+ThatMarko@users.noreply.github.com>
2023-04-19 13:45:24 -0500

    Add links to the buttons.

--- src/components/MobileSMBubbles/index.jsx
+++ src/components/MobileSMBubbles/index.jsx
@@ -6,23 +6,23 @@ import {
   YoutubeSMBubble,
 } from "../SocialMediaBubbles";
 
-export default function MobileSMBubbles() {
+export default function MobileSMBubbles({links}) {
   return (
     <div className="flex flex-col -space-y-16 ">
       <div className=" items-center justify-center inline-flex z-0">
-        <InstagramSMBubble />
+        <InstagramSMBubble link = {links.Instagram} />
       </div>
       <div className=" items-center justify-center inline-flex z-10">
-        <LinkedinSMBubble />
+        <LinkedinSMBubble link = {links.LinkedIn} />
       </div>
       <div className=" items-center justify-center inline-flex z-20">
-        <YoutubeSMBubble />
+        <YoutubeSMBubble link = {links.YouTube} />
       </div>
       <div className=" items-center justify-center inline-flex z-30">
-        <TwitterSMBubble />
+        <TwitterSMBubble link = {links.Twitter} />
       </div>
       <div className=" items-center justify-center inline-flex z-40">
-        <FacebookSMBubble />
+        <FacebookSMBubble link = {links.Facebook}/>
       </div>
     </div>
   );
--- src/pages/social.js
+++ src/pages/social.js
@@ -4,16 +4,18 @@ import DesktopNewsletter from "../components/DesktopNewsletter";
 import MobileSMBubbles from "../components/MobileSMBubbles";
 import MobileNewsletter from "../components/MobileNewsletter"
 
+import SocialLinks from "../api/SocialMediaLinks";
+
 export default function Social() {
   return (
     <div>
       <div className="hidden lg:block">
         <DesktopNewsletter />
-        <DesktopSMBubbles />
+        <DesktopSMBubbles links={SocialLinks} />
       </div>
       <div className="lg:hidden">
         <MobileNewsletter />
-        <MobileSMBubbles />
+        <MobileSMBubbles links={SocialLinks} />
       </div>
     </div>
   );
