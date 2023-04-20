9d6be6f70722cfa07828f215652b6c40745d5798
Goutham <gouthamkumarpolapally@gmail.com>
2023-04-20 11:27:36 +0530

    Removed MUI Packages for alert and designed it with custom css

--- src/components/MobileNewsletter/index.js
+++ src/components/MobileNewsletter/index.js
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import PrimaryButton from '../PrimaryButton'
-import { Alert } from '@mui/material'
+import PrimaryButton from '../PrimaryButton';
+import "./MobileNewsletter.css";
 export default function MobileNewsletter() {
   const[showAlert,handleShowAlert] = useState(false);
   let email = ""
@@ -10,14 +10,13 @@ export default function MobileNewsletter() {
   }
   return (
     <div className='text-center space-y-3 mx-2'>
-    {showAlert ? <>
-      <Alert
-    severity='info'
-    className="my-3"
-    onClose={()=>{handleShowAlert(false)}}>
-      Our newsletter sign ups has closed for this event! We hope to see you next year!
-    </Alert>
-    </> :<></>}
+  {showAlert? <>
+            <div className="alert-card"
+    >
+      <p className="alert-info">Our newsletter sign ups has closed for this event! We hope to see you next year!</p>
+      <p className="closeBtn" onClick={()=>{handleShowAlert(false)}}>X</p>
+    </div>
+          </> :<></>}
       <h1>Join Our Newsletter!</h1>
       <p>Want to get updates as fast as possible! Join our newsletter! We will send details about the event with sign up and RSVP forms as we get closer to the start date! We will also send insights into what types of events we are planning, and much more! Sign up to make sure you get the latest as soon as possible!</p>
       <div className='space-y-2'>
