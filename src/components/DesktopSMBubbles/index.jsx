c5c8b7153d98f26b50937576a19b645aa85a260e
Marko Bogosavljevic <7422135+ThatMarko@users.noreply.github.com>
2023-08-22 15:04:09 -0500

    ThatMarko/issue223 (#224)
    * Add central file that contains all the social media links
    
    * Update footer
    
    * Modify SocialMediaBubbles to accept link and be clickable.
    
    * Update SocialMediaBubbles with links

--- src/components/DesktopSMBubbles/index.jsx
+++ src/components/DesktopSMBubbles/index.jsx
@@ -1,19 +1,20 @@
 import { FacebookSMBubble, InstagramSMBubble, LinkedinSMBubble, TwitterSMBubble, YoutubeSMBubble } from "../SocialMediaBubbles";
 
-export default function DesktopSMBubbles() {
-    return( 
+export default function DesktopSMBubbles({links}) {
+    console.log(links)
+    return(
         <div className="grid h-screen place-items-center">  
             <div className="relative items-center justify-center inline-flex w-7/12" >
                 <div className="relative items-center justify-center inline-flex z-0">
-                    <InstagramSMBubble/>
+                    <InstagramSMBubble link = {links.Instagram} />
                         <div className="relative items-center justify-center inline-flex w-5/12 z-10">
-                            <LinkedinSMBubble/>
+                            <LinkedinSMBubble link = {links.LinkedIn}/>
                             <div className="relative items-center justify-center inline-flex w-3/12 z-20">
-                                <YoutubeSMBubble/>
+                                <YoutubeSMBubble link = {links.YouTube}/>
                             </div>
-                            <TwitterSMBubble/>
+                            <TwitterSMBubble link = {links.Twitter}/>
                         </div>
-                    <FacebookSMBubble/>
+                    <FacebookSMBubble link = {links.Facebook}/>
                 </div>
             </div>
         </div>
