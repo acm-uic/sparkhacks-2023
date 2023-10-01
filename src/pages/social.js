068be0cfc85317108bf0ff1581f5d98228d14586
Tanishq Mishra <mishratanishq619@gmail.com>
2023-10-01 13:17:42 +0530

    Fixed : Too much spacing #199

--- src/pages/social.js
+++ src/pages/social.js
@@ -2,21 +2,21 @@ import "./social.css";
 import DesktopSMBubbles from "../components/DesktopSMBubbles";
 import DesktopNewsletter from "../components/DesktopNewsletter";
 import MobileSMBubbles from "../components/MobileSMBubbles";
-import MobileNewsletter from "../components/MobileNewsletter"
+import MobileNewsletter from "../components/MobileNewsletter";
 
 import SocialLinks from "../api/SocialMediaLinks";
 
 export default function Social() {
-  return (
-    <div>
-      <div className="hidden lg:block">
-        <DesktopNewsletter />
-        <DesktopSMBubbles links={SocialLinks} />
-      </div>
-      <div className="lg:hidden">
-        <MobileNewsletter />
-        <MobileSMBubbles links={SocialLinks} />
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<div className="hidden lg:block">
+				<DesktopSMBubbles links={SocialLinks} />
+				<DesktopNewsletter />
+			</div>
+			<div className="lg:hidden">
+				<MobileSMBubbles links={SocialLinks} />
+				<MobileNewsletter />
+			</div>
+		</div>
+	);
 }
